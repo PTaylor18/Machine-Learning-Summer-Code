30070ae05383cc4504c7d96d3accf7d9bc6f4a9d
Paul Taylor <78972614+PTaylor18@users.noreply.github.com>
2021-08-12 14:45:50 +0100

    Mostly works

--- Quantum/Classification/1D and 2D Data/1D Classification Model.jl
+++ Quantum/Classification/1D and 2D Data/1D Classification Model.jl
@@ -4,6 +4,7 @@ using QuAlgorithmZoo: Adam, update!
 using CSV
 using DataFrames
 using Plots
+using StatsPlots
 #using Flux
 
 # read in data
@@ -14,6 +15,18 @@ train = one_d_data[!, Not(:label)]
 train = train[!,1]
 labels = one_d_data[!, Not(:x)]
 labels = labels[!,1]
+
+# plot data
+gdf = groupby(one_d_data, :2);
+plot(gdf[2].x, gdf[2].label, seriestype=:scatter, color=:red, label='0')
+plot!(gdf[1].x, gdf[1].label, seriestype=:scatter, color=:blue, label='1')
+
+# split into train and test data
+test = splice!(train, 20:59)
+test_labels = splice!(labels, 20:59)
+
+train
+labels
 
 #replace!(y, 0=>-1)
 
@@ -32,6 +45,8 @@ sz1 = chain(N, put(1=>Z))
 #---
 
 sigmoid(x) = 1 ./(1. +exp(.-x))
+relu(x) = max(0.5, x)
+softmax(x) =
 
 optimizer = Adam(lr=0.01)
 d = 6; # QNN depth
@@ -50,32 +65,35 @@ for j = 1:niter
         dCdθ = expect'(cost, (zero_state(N) |> feature_map(N, train[i])) => Uθ).second; # gradient of <C>
         grad_params += 2. * ((expect(cost, (zero_state(N) |> feature_map(N, train[i])) => Uθ) - labels[i]) |> real) * dCdθ; # full loss function grads
     end
+    update!
     # feed the gradients into the circuit
     dispatch!(Uθ, update!(params, grad_params, optimizer))
     loss = 0.; # total cost
     for i=1:length(train)
-        loss += (expect(cost, (zero_state(N) |> feature_map(N, train[i])) |> Uθ) - labels[i])^2 |> real;
+        loss += (-1 * labels[i] * log(expect(cost, (zero_state(N) |> feature_map(N, train[i])) |> Uθ)) |> real); #binary cross entropy
+        #loss += (expect(cost, (zero_state(N) |> feature_map(N, train[i])) |> Uθ) - labels[i])^2 |> real;
     end
-
+    loss = loss * (1/length(train))
     println("Step $j, loss = $loss "); flush(Core.stdout)
     append!(loss_vec, loss)
 end
-
-
 
 #plot the losses at each epoch
 epoch = 1:niter
 plot(epoch, loss_vec, xaxis=("Epoch"), yaxis=("Loss"))
 
 solution = Vector{Float64}()
-for i=1:length(train)
+for i=1:length(test)
     dispatch!(Uθ, params)
-    append!(solution, (expect(cost, (zero_state(N) |> feature_map(N, train[i])) |> Uθ)) |> real)
+    append!(solution, sigmoid((expect(cost, (zero_state(N) |> feature_map(N, test[i])) |> Uθ) |> real)))
 end
 println(solution)
 solution
 
-# plot the solution
+scatter!(train, solution)
+scatter!(test, solution)
+
+# plot the classified data
 fplot = plot!(tlist, solution, xaxis = ("t"), yaxis = ("f(t)"), label = "QNN fit", markersize = 6, c = :blue, marker = "x")
 
 println("Finished")
